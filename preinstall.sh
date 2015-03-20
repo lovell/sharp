711f0fefb674c3048edcd86cbbaad1b209fd3df4
Lovell Fuller <github@lovell.info>
2015-03-20 14:57:13 +0000

    Disable unused static library and introspection components

--- preinstall.sh
+++ preinstall.sh
@@ -25,7 +25,7 @@ install_libvips_from_source() {
   curl -O http://www.vips.ecs.soton.ac.uk/supported/$vips_version_latest_major_minor/vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
   tar zvxf vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
   cd vips-$vips_version_latest_major_minor.$vips_version_latest_patch
-  ./configure --enable-debug=no --enable-docs=no --enable-cxx=yes --without-python --without-orc --without-fftw $1
+  ./configure --disable-debug --disable-docs --disable-static --disable-introspection --enable-cxx=yes --without-python --without-orc --without-fftw $1
   make
   make install
   cd ..
