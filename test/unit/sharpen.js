692e2d4df48cf394c753cd226c2d8cc03d9bf7ca
Lovell Fuller <github@lovell.info>
2015-09-07 14:17:35 +0100

    Automate expected vs actual for blur/sharpen tests

--- test/unit/sharpen.js
+++ test/unit/sharpen.js
@@ -13,12 +13,11 @@ describe('Sharpen', function() {
     sharp(fixtures.inputJpg)
       .resize(320, 240)
       .sharpen(10)
-      .toFile(fixtures.path('output.sharpen-10.jpg'), function(err, info) {
-        if (err) throw err;
+      .toBuffer(function(err, data, info) {
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
         assert.strictEqual(240, info.height);
-        done();
+        fixtures.assertSimilar(fixtures.expected('sharpen-10.jpg'), data, done);
       });
   });
 
@@ -26,12 +25,11 @@ describe('Sharpen', function() {
     sharp(fixtures.inputJpg)
       .resize(320, 240)
       .sharpen(3, 0.5, 2.5)
-      .toFile(fixtures.path('output.sharpen-3-0.5-2.5.jpg'), function(err, info) {
-        if (err) throw err;
+      .toBuffer(function(err, data, info) {
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
         assert.strictEqual(240, info.height);
-        done();
+        fixtures.assertSimilar(fixtures.expected('sharpen-3-0.5-2.5.jpg'), data, done);
       });
   });
 
@@ -39,12 +37,11 @@ describe('Sharpen', function() {
     sharp(fixtures.inputJpg)
       .resize(320, 240)
       .sharpen(5, 2, 4)
-      .toFile(fixtures.path('output.sharpen-5-2-4.jpg'), function(err, info) {
-        if (err) throw err;
+      .toBuffer(function(err, data, info) {
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
         assert.strictEqual(240, info.height);
-        done();
+        fixtures.assertSimilar(fixtures.expected('sharpen-5-2-4.jpg'), data, done);
       });
   });
 
@@ -52,46 +49,30 @@ describe('Sharpen', function() {
     sharp(fixtures.inputJpg)
       .resize(320, 240)
       .sharpen()
-      .toFile(fixtures.path('output.sharpen-mild.jpg'), function(err, info) {
-        if (err) throw err;
+      .toBuffer(function(err, data, info) {
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
         assert.strictEqual(240, info.height);
-        done();
+        fixtures.assertSimilar(fixtures.expected('sharpen-mild.jpg'), data, done);
       });
   });
 
-  it('invalid radius', function(done) {
-    var isValid = true;
-    try {
+  it('invalid radius', function() {
+    assert.throws(function() {
       sharp(fixtures.inputJpg).sharpen(1.5);
-    } catch (err) {
-      isValid = false;
-    }
-    assert.strictEqual(false, isValid);
-    done();
+    });
   });
 
-  it('invalid flat', function(done) {
-    var isValid = true;
-    try {
+  it('invalid flat', function() {
+    assert.throws(function() {
       sharp(fixtures.inputJpg).sharpen(1, -1);
-    } catch (err) {
-      isValid = false;
-    }
-    assert.strictEqual(false, isValid);
-    done();
+    });
   });
 
-  it('invalid jagged', function(done) {
-    var isValid = true;
-    try {
+  it('invalid jagged', function() {
+    assert.throws(function() {
       sharp(fixtures.inputJpg).sharpen(1, 1, -1);
-    } catch (err) {
-      isValid = false;
-    }
-    assert.strictEqual(false, isValid);
-    done();
+    });
   });
 
   it('sharpened image is larger than non-sharpened', function(done) {
@@ -99,7 +80,6 @@ describe('Sharpen', function() {
       .resize(320, 240)
       .sharpen(false)
       .toBuffer(function(err, notSharpened, info) {
-        if (err) throw err;
         assert.strictEqual(true, notSharpened.length > 0);
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
@@ -108,7 +88,6 @@ describe('Sharpen', function() {
           .resize(320, 240)
           .sharpen(true)
           .toBuffer(function(err, sharpened, info) {
-            if (err) throw err;
             assert.strictEqual(true, sharpened.length > 0);
             assert.strictEqual(true, sharpened.length > notSharpened.length);
             assert.strictEqual('jpeg', info.format);
