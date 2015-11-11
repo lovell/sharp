7383596f8c65f2422a50de2c87a43644d417647f
Lovell Fuller <github@lovell.info>
2015-11-11 21:37:04 +0000

    Allow tty-less docker-inside-docker usage

--- packaging/test/opensuse.sh
+++ packaging/test/opensuse.sh
@@ -4,4 +4,4 @@
 zypper addrepo http://download.opensuse.org/repositories/devel:languages:nodejs/openSUSE_13.2/devel:languages:nodejs.repo
 zypper --gpg-auto-import-keys refresh
 zypper --non-interactive install gcc-c++ make nodejs-devel
-npm install -g npm
+/usr/bin/npm install -g npm@latest
