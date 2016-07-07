86f9376a221f889f9579a3ecc89dc4a8a8fb907e
Matt Hirsch <matthew.hirsch@gmail.com>
2016-07-06 20:47:51 -0400

    Add a unit test for extract area out of bounds

--- test/unit/extract.js
+++ test/unit/extract.js
@@ -175,5 +175,11 @@ describe('Partial image extraction', function() {
         sharp(fixtures.inputJpg).extract({ left: 10, top: 10, width: 10, height: null });
       });
     });
+
+    it('Bad image area', function() {
+      assert.throws(function() {
+        sharp(fixtures.inputJpg).extract({ left: 3000, top: 10, width: 10, height: 10 });
+      });
+    });
   });
 });
