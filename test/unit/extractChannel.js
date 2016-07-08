339f7a673c022d2cace4633984000d3b78b62f50
Matt Hirsch <matthew.hirsch@gmail.com>
2016-07-08 11:30:49 -0400

    Add unit tests for each of the named color channels

--- test/unit/extractChannel.js
+++ test/unit/extractChannel.js
@@ -19,7 +19,31 @@ describe('Image channel extraction', function() {
       });
   });
 
+  it('Green channel', function(done) {
+    sharp(fixtures.inputJpg)
+      .extractChannel('green')
+      .resize(320,240)
+      .toBuffer(function(err, data, info) {
+        if (err) throw err;
+        assert.strictEqual(320, info.width);
+        assert.strictEqual(240, info.height);
+        fixtures.assertSimilar(fixtures.expected('extract-green.jpg'), data, { threshold: 8 }, done);
+      });
+  });
+
   it('Blue channel', function(done) {
+    sharp(fixtures.inputJpg)
+      .extractChannel('blue')
+      .resize(320,240)
+      .toBuffer(function(err, data, info) {
+        if (err) throw err;
+        assert.strictEqual(320, info.width);
+        assert.strictEqual(240, info.height);
+        fixtures.assertSimilar(fixtures.expected('extract-blue.jpg'), data, { threshold: 8 }, done);
+      });
+  });
+
+  it('Blue channel by number', function(done) {
     sharp(fixtures.inputJpg)
       .extractChannel(2)
       .resize(320,240)
