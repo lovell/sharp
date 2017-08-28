f454cdbc7de9197406d67f2ee91e9693bceae34e
Matthew McEachen <matthew+github@mceachen.org>
2017-08-28 11:00:16 -0700

    add assertions for punch-corrupted images

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -67,6 +67,7 @@ module.exports = {
   inputJpg320x240: getPath('320x240.jpg'), // http://www.andrewault.net/2010/01/26/create-a-test-pattern-video-with-perl/
   inputJpgOverlayLayer2: getPath('alpha-layer-2-ink.jpg'),
   inputJpgTruncated: getPath('truncated.jpg'), // head -c 10000 2569067123_aca715a2ee_o.jpg > truncated.jpg
+  inputJpgPunched: getPath('punched.jpg'), // cp 320x240.jpg punched.jpg && punch punched.jpg 16
 
   inputPng: getPath('50020484-00001.png'), // http://c.searspartsdirect.com/lis_png/PLDM/50020484-00001.png
   inputPngWithTransparency: getPath('blackbug.png'), // public domain
--- test/unit/failOnError.js
+++ test/unit/failOnError.js
@@ -5,16 +5,28 @@ const assert = require('assert');
 const sharp = require('../../');
 const fixtures = require('../fixtures');
 
-describe('failOnError', function () {
+describe.only('failOnError', function () {
   it('handles truncated images by default', function (done) {
     sharp(fixtures.inputJpgTruncated)
       .resize(320, 240)
+      // .toFile(fixtures.expected('truncated.jpg'), done);
       .toBuffer(function (err, data, info) {
         if (err) throw err;
         assert.strictEqual('jpeg', info.format);
         assert.strictEqual(320, info.width);
         assert.strictEqual(240, info.height);
         fixtures.assertSimilar(fixtures.expected('truncated.jpg'), data, done);
+      });
+  });
+
+  it('handles zero-punched corrupted images', function (done) {
+    sharp(fixtures.inputJpgPunched)
+      .toBuffer(function (err, data, info) {
+        if (err) throw err;
+        assert.strictEqual('jpeg', info.format);
+        assert.strictEqual(320, info.width);
+        assert.strictEqual(240, info.height);
+        fixtures.assertSimilar(fixtures.expected('punched.jpg'), data, done);
       });
   });
 
@@ -34,20 +46,39 @@ describe('failOnError', function () {
 
   it('returns errors to callback for truncated images when failOnError is set', function (done) {
     sharp(fixtures.inputJpgTruncated, { failOnError: true })
-      .resize(320, 240)
       .toBuffer(function (err, data, info) {
-        assert.equal(err, null);
-        assert.equal(data.byteLength, 0);
+        console.dir({err, info});  // TODO(mceachen): delete
+        assert.equal(err, 'Error: Premature end of JPEG file');
+        assert.equal(data, null);
+        assert.equal(info.size, 0);
+        done();
+      });
+    });
+    
+  it('returns errors to callback for zero-punched images when failOnError is set', function (done) {
+      sharp(fixtures.inputJpgPunched, { failOnError: true })
+      .toBuffer(function (err, data, info) {
+        console.dir({err, info});  // TODO(mceachen): delete
+        assert.equal(err, 'Error: Corrupt JPEG data: 66 extraneous bytes before marker 0xd9');
+        assert.equal(data, null);
+        assert.equal(info.size, 0);
         done();
       });
   });
 
   it('rejects promises for truncated images when failOnError is set', function () {
-    return sharp(fixtures.inputJpgTruncated, { failOnError: true })
-      .resize(320, 240)
-      .toBuffer()
-      .then(buffer => {
-        assert.equal(buffer.byteLength, 0);
+    return new Promise(function (resolve, reject) {
+      sharp(fixtures.inputJpgTruncated, { failOnError: true })
+        .resize(320, 240)
+        .toBuffer()
+        .then(function () {
+          reject(new Error('Error was expected'));
+        })
+      .catch(function (err) {
+        console.log('Caught expected error ' + expected);  // TODO(mceachen): delete
+        assert.include(err.toString(), "Premature end of JPEG file");
+        resolve();
       });
+    });
   });
 });
