36bdf2abcc1db2d587b3842dea125e7c111fbcb9
Lovell Fuller <github@lovell.info>
2017-09-26 19:06:40 +0100

    Verify platform of vendor binaries at install and run time

--- binding.js
+++ binding.js
@@ -10,23 +10,14 @@ const semver = require('semver');
 const tar = require('tar');
 const detectLibc = require('detect-libc');
 
+const platform = require('./lib/platform');
+
 // Use NPM-provided environment variable where available, falling back to require-based method for Electron
 const minimumLibvipsVersion = process.env.npm_package_config_libvips || require('./package.json').config.libvips;
 
 const distBaseUrl = process.env.SHARP_DIST_BASE_URL || `https://github.com/lovell/sharp-libvips/releases/download/v${minimumLibvipsVersion}/`;
 
-const platform = process.env.npm_config_platform || process.platform;
-
-const arch = process.env.npm_config_arch || process.arch;
-
 // -- Helpers
-
-// Does this file exist?
-const isFile = function (file) {
-  try {
-    return fs.statSync(file).isFile();
-  } catch (err) {}
-};
 
 const unpack = function (tarPath, done) {
   const vendorPath = path.join(__dirname, 'vendor');
@@ -41,73 +32,68 @@ const unpack = function (tarPath, done) {
     .catch(error);
 };
 
-const platformId = function () {
-  const platformId = [platform];
-  if (arch === 'arm' || arch === 'armhf' || arch === 'arm64') {
-    const armVersion = (arch === 'arm64') ? '8' : process.env.npm_config_armv || process.config.variables.arm_version || '6';
-    platformId.push('armv' + armVersion);
-  } else {
-    platformId.push(arch);
-  }
-  return platformId.join('-');
-};
-
-// Error
 const error = function (msg) {
   if (msg instanceof Error) {
     msg = msg.message;
   }
-  process.stderr.write('ERROR: ' + msg + '\n');
+  process.stderr.write(`sharp: ${msg}\n`);
   process.exit(1);
 };
 
 // -- Binary downloaders
 
 module.exports.download_vips = function () {
-  // Has vips been installed locally?
-  const vipsHeaderPath = path.join(__dirname, 'vendor', 'include', 'vips', 'vips.h');
-  if (!isFile(vipsHeaderPath)) {
-    // Ensure Intel 64-bit or ARM
-    if (arch === 'ia32') {
-      error('Intel Architecture 32-bit systems require manual installation of libvips - please see http://sharp.dimens.io/page/install');
+  // Check for existing vendored binaries and verify platform matches
+  const currentPlatformId = platform();
+  try {
+    const vendorPlatformId = require(path.join(__dirname, 'vendor', 'platform.json'));
+    if (currentPlatformId === vendorPlatformId) {
+      return;
+    } else {
+      error(`'${vendorPlatformId}' binaries cannot be used on the '${currentPlatformId}' platform. Please remove the 'node_modules/sharp/vendor' directory and run 'npm install'.`);
     }
-    // Ensure glibc Linux
-    if (detectLibc.isNonGlibcLinux) {
-      error(`Use with ${detectLibc.family} libc requires manual installation of libvips - please see http://sharp.dimens.io/page/install`);
-    }
-    // Ensure glibc >= 2.13
-    if (detectLibc.family === detectLibc.GLIBC && detectLibc.version && semver.lt(`${detectLibc.version}.0`, '2.13.0')) {
-      error(`Use with glibc version ${detectLibc.version} requires manual installation of libvips - please see http://sharp.dimens.io/page/install`);
-    }
-    // Arch/platform-specific .tar.gz
-    const tarFilename = ['libvips', minimumLibvipsVersion, platformId()].join('-') + '.tar.gz';
-    // Download to per-process temporary file
-    const tarPathTemp = path.join(os.tmpdir(), process.pid + '-' + tarFilename);
-    const tmpFile = fs.createWriteStream(tarPathTemp).on('finish', function () {
-      unpack(tarPathTemp, function () {
-        // Attempt to remove temporary file
-        try {
-          fs.unlinkSync(tarPathTemp);
-        } catch (err) {}
-      });
-    });
-    const url = distBaseUrl + tarFilename;
-    const simpleGetOpt = {
-      url: url,
-      agent: caw(null, {
-        protocol: 'https'
-      })
-    };
-    simpleGet(simpleGetOpt, function (err, response) {
-      if (err) {
-        error('Download of ' + url + ' failed: ' + err.message);
-      }
-      if (response.statusCode !== 200) {
-        error(url + ' status code ' + response.statusCode);
-      }
-      response.pipe(tmpFile);
-    });
+  } catch (err) {}
+  // Ensure Intel 64-bit or ARM
+  const arch = process.env.npm_config_arch || process.arch;
+  if (arch === 'ia32') {
+    error('Intel Architecture 32-bit systems require manual installation of libvips - please see http://sharp.dimens.io/page/install');
   }
+  // Ensure glibc Linux
+  if (detectLibc.isNonGlibcLinux) {
+    error(`Use with ${detectLibc.family} libc requires manual installation of libvips - please see http://sharp.dimens.io/page/install`);
+  }
+  // Ensure glibc >= 2.13
+  if (detectLibc.family === detectLibc.GLIBC && detectLibc.version && semver.lt(`${detectLibc.version}.0`, '2.13.0')) {
+    error(`Use with glibc version ${detectLibc.version} requires manual installation of libvips - please see http://sharp.dimens.io/page/install`);
+  }
+  // Arch/platform-specific .tar.gz
+  const tarFilename = ['libvips', minimumLibvipsVersion, currentPlatformId].join('-') + '.tar.gz';
+  // Download to per-process temporary file
+  const tarPathTemp = path.join(os.tmpdir(), `${process.pid}-${tarFilename}`);
+  const tmpFile = fs.createWriteStream(tarPathTemp).on('finish', function () {
+    unpack(tarPathTemp, function () {
+      // Attempt to remove temporary file
+      try {
+        fs.unlinkSync(tarPathTemp);
+      } catch (err) {}
+    });
+  });
+  const url = distBaseUrl + tarFilename;
+  const simpleGetOpt = {
+    url: url,
+    agent: caw(null, {
+      protocol: 'https'
+    })
+  };
+  simpleGet(simpleGetOpt, function (err, response) {
+    if (err) {
+      error(`${url} download failed: ${err.message}`);
+    }
+    if (response.statusCode !== 200) {
+      error(`${url} download failed: status ${response.statusCode}`);
+    }
+    response.pipe(tmpFile);
+  });
 };
 
 module.exports.use_global_vips = function () {
