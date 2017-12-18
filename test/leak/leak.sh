13997ca6537076660d470cd1b69db85e096374c5
Lovell Fuller <github@lovell.info>
2017-12-18 19:57:26 +0000

    Run unit test files sequentially through valgrind

--- test/leak/leak.sh
+++ test/leak/leak.sh
@@ -7,12 +7,14 @@ fi
 
 curl -o ./test/leak/libvips.supp https://raw.githubusercontent.com/jcupitt/libvips/master/libvips.supp
 
-G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
-  --suppressions=test/leak/libvips.supp \
-  --suppressions=test/leak/sharp.supp \
-  --gen-suppressions=yes \
-  --leak-check=full \
-  --show-leak-kinds=definite,indirect,possible \
-  --num-callers=20 \
-  --trace-children=yes \
-  npm test
+for test in ./test/unit/*.js; do
+  G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
+    --suppressions=test/leak/libvips.supp \
+    --suppressions=test/leak/sharp.supp \
+    --gen-suppressions=yes \
+    --leak-check=full \
+    --show-leak-kinds=definite,indirect,possible \
+    --num-callers=20 \
+    --trace-children=yes \
+    mocha --slow=60000 --timeout=120000 "$test";
+done
