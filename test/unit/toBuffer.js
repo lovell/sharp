f90204e559b75fe87faa609ce9415b319faa4314
Jordan Prudhomme <j.prudhomme@bee-buzziness.com>
2019-09-05 09:30:35 +0200

    reusing same sharp instance and differente options to toBuffer do not produce the same output

---
+++ test/unit/toBuffer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const assert = require('assert');
+
+const sharp = require('../../');
+const fixtures = require('../fixtures');
+
+describe('toBuffer', () => {
+  it('reusing same sharp object does not reset previously passed parameters to toBuffer', (done) => {
+    let image = sharp(fixtures.inputJpg);
+    image.toBuffer({ resolveWithObject: true }).then((obj) => {
+      image.toBuffer().then((buff) => {
+        assert.strict.equal(Buffer.isBuffer(buff), true);
+        assert.strict.equal(typeof obj, 'object');
+        done();
+      });
+    });
+  });
+});
