9baf38db440cb3f300808095bec7e5bc138b8b32
Pouya Eghbali <pouya.eghbali@yandex.com>
2019-11-07 00:11:44 +0100

    Allow compilation of v0.23.x on FreeBSD and variants (#1952)

--- src/pipeline.cc
+++ src/pipeline.cc
@@ -38,6 +38,9 @@
 #elif defined(__APPLE__)
 #define STAT64_STRUCT stat
 #define STAT64_FUNCTION stat
+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+#define STAT64_STRUCT stat
+#define STAT64_FUNCTION stat
 #else
 #define STAT64_STRUCT stat64
 #define STAT64_FUNCTION stat64
