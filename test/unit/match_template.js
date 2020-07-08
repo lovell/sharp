52ee73e33d3d16f42fa1d6797defe95fa9c69f10
Aaron Podolny <apodolny@gmail.com>
2020-07-08 16:01:52 -0700

    Add additional match template test, restore unit testing in package.json.

--- test/unit/match_template.js
+++ test/unit/match_template.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 const sharp = require('../../lib');
 const fixtures = require('../fixtures');
 
-describe('Match template operation', function () {
-  it('Ref image appears in image', function (done) {
+describe('Match Template', function () {
+  it('Template image appears in image', function (done) {
     sharp(fixtures.inputJpgWithLandscapeExif1)
       .matchTemplate(fixtures.inputJpgFlowerCroppedFromLandscape1).then((results) => {
         assert.strictEqual(61597, results.min);
@@ -17,7 +17,7 @@ describe('Match template operation', function () {
       });
   });
 
-  it('Ref image does not appear in image', function (done) {
+  it('Template image does not appear in image', function (done) {
     sharp(fixtures.inputJpg320x240)
       .matchTemplate(fixtures.inputJpgFlowerCroppedFromLandscape1).then((results) => {
         assert.strictEqual(14667082, results.min);
@@ -25,6 +25,18 @@ describe('Match template operation', function () {
         assert.strictEqual(239, results.minY);
         assert.strictEqual(28271984, results.referenceSumSquares);
         assert.strictEqual(0.6360911144067776, results.score);
+        done();
+      });
+  });
+
+  it('Template image is original image should be perfect match', function (done) {
+    sharp(fixtures.inputJpgFlowerCroppedFromLandscape1)
+      .matchTemplate(fixtures.inputJpgFlowerCroppedFromLandscape1).then((results) => {
+        assert.strictEqual(0, results.min);
+        assert.strictEqual(27, results.minX);
+        assert.strictEqual(23, results.minY);
+        assert.strictEqual(28271984, results.referenceSumSquares);
+        assert.strictEqual(1.0, results.score);
         done();
       });
   });
