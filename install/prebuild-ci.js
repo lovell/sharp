b7116617841a80a7959b7b0bda84776bc3cb0638
Lovell Fuller <github@lovell.info>
2020-08-18 17:11:42 +0100

    CI: improve cross-platform (i.e. Windows) process spawning

--- install/prebuild-ci.js
+++ install/prebuild-ci.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const { execFileSync } = require('child_process');
+const { spawnSync } = require('child_process');
 
 const { prebuild_upload: hasToken, APPVEYOR_REPO_TAG, TRAVIS_TAG } = process.env;
 
 if (hasToken && (Boolean(APPVEYOR_REPO_TAG) || TRAVIS_TAG)) {
-  execFileSync('prebuild', ['--runtime', 'napi', '--target', '3'], { stdio: 'inherit' });
+  spawnSync(
+    'node ./node_modules/.bin/prebuild --runtime napi --target 3',
+    { shell: true, stdio: 'inherit' }
+  );
 }
