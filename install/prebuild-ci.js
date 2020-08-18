0c4a45b1f427f30d37169191ffdcbe9768866bff
Lovell Fuller <github@lovell.info>
2020-08-18 14:31:33 +0100

    CI: workaround Appveyor ignoring v-prefixed tag names

--- install/prebuild-ci.js
+++ install/prebuild-ci.js
@@ -2,8 +2,8 @@
 
 const { execFileSync } = require('child_process');
 
-const { prebuild_upload: hasToken, APPVEYOR_REPO_TAG_NAME, TRAVIS_TAG } = process.env;
+const { prebuild_upload: hasToken, APPVEYOR_REPO_TAG, TRAVIS_TAG } = process.env;
 
-if (hasToken && (APPVEYOR_REPO_TAG_NAME || TRAVIS_TAG)) {
+if (hasToken && (Boolean(APPVEYOR_REPO_TAG) || TRAVIS_TAG)) {
   execFileSync('prebuild', ['--runtime', 'napi', '--target', '3'], { stdio: 'inherit' });
 }
