05ca7d31290c2be2015204a8137d517b780dd9d2
Lovell Fuller <github@lovell.info>
2020-08-18 20:42:44 +0100

    CI: further attempts to get Windows to play nicely

--- install/prebuild-ci.js
+++ install/prebuild-ci.js
@@ -2,11 +2,11 @@
 
 const { spawnSync } = require('child_process');
 
-const { prebuild_upload: hasToken, APPVEYOR_REPO_TAG, TRAVIS_TAG } = process.env;
+const { prebuild_upload: hasToken, APPVEYOR_REPO_TAG_NAME, TRAVIS_TAG } = process.env;
 
-if (hasToken && (Boolean(APPVEYOR_REPO_TAG) || TRAVIS_TAG)) {
-  spawnSync(
-    'node ./node_modules/.bin/prebuild --runtime napi --target 3',
+if (hasToken && (APPVEYOR_REPO_TAG_NAME || TRAVIS_TAG)) {
+  spawnSync('node',
+    ['./node_modules/prebuild/bin.js', '--runtime', 'napi', '--target', '3'],
     { shell: true, stdio: 'inherit' }
   );
 }
