9cc8db5aca3d4e1b62599987da43f3d9ead54bb1
Leon Radley <leon@radley.se>
2021-01-04 20:56:22 +0100

    Rewrote tests to use async await and made more assertions
    Removed nested then calls and used async await instead.
    
    Also fixed bug where Buffer objects will always satisfy the instanceof Uint8Array checks.

--- test/unit/toUint8Array.js
+++ test/unit/toUint8Array.js
@@ -13,91 +13,58 @@ describe('toUint8Array', () => {
     sharp.cache(true);
   });
 
-  it('should output data as Uint8Array', (done) => {
-    const image = sharp(fixtures.inputJpg320x240);
-    image.toUint8Array().then((data) => {
-      assert.strictEqual(data instanceof Uint8Array, true);
-      assert.strictEqual(data.length, 26641);
-      done();
-    });
+  it('should output data as Uint8Array', async () => {
+    const data = await sharp(fixtures.inputJpg320x240).toUint8Array()
+    assert.strictEqual(data.constructor.name, 'Uint8Array');
+    assert.strictEqual(data.length, 26641);
   });
 
-  it('should output data as Uint8Array with resolveWithObject: true', (done) => {
-    const image = sharp(fixtures.inputJpg320x240);
-    image.toUint8Array({ resolveWithObject: true }).then((res) => {
-      assert.strictEqual(res.data instanceof Uint8Array, true);
-      assert.strictEqual(res.data.length, 26641);
-      assert.strictEqual(res.info.width, 320);
-      done();
-    });
+  it('should output data as Uint8Array with resolveWithObject: true', async () => {
+    const { data, info } = await sharp(fixtures.inputJpg320x240).toUint8Array({ resolveWithObject: true });
+    assert.strictEqual(data.constructor.name, 'Uint8Array');
+    assert.strictEqual(data.length, 26641);
+    assert.strictEqual(info.width, 320);
   });
 
   it('should handle toUint8Array callback', (done) => {
-    const image = sharp(fixtures.inputJpg320x240);
-    image.toUint8Array((err, data, info) => {
+    sharp(fixtures.inputJpg320x240).toUint8Array((err, data, info) => {
       if (err) throw err;
-      assert.strictEqual(data instanceof Uint8Array, true);
+      assert.strictEqual(data.constructor.name, 'Uint8Array');
       assert.strictEqual(data.length, 26641);
       assert.strictEqual(info.size, 26641);
       done();
     });
   });
 
-  it('should take a Uint8Array as an input', (done) => {
-    const image = sharp(fixtures.inputJpg320x240);
-    image.toUint8Array().then((data) => {
-      sharp(data).metadata().then(info => {
-        assert.strictEqual(info.width, 320);
-        assert.strictEqual(info.height, 240);
-        done();
-      });
-    });
+  it('should take a Uint8Array as an input and output same data', async () => {
+    const uint8array = Uint8Array.from([255, 255, 255, 0, 0, 0]);
+    const { data, info } = await sharp(uint8array, {
+      raw: {
+        width: 2,
+        height: 1,
+        channels: 3
+      }
+    }).toUint8Array({ resolveWithObject: true });
+
+    assert.deepStrictEqual(uint8array, data);
+    assert.strictEqual(info.width, 2);
+    assert.strictEqual(info.height, 1);
   });
 
-  it('should take a Uint8ClampedArray as an input', (done) => {
+  it('should take a Uint8ClampedArray as an input and output same data', async () => {
     // since a Uint8ClampedArray is the same as Uint8Array but clamps the values
     // between 0-255 it seemed good to add this also
     const uint8array = Uint8ClampedArray.from([255, 255, 255, 0, 0, 0]);
-    sharp(uint8array, {
+    const { data, info } = await sharp(uint8array, {
       raw: {
         width: 2,
         height: 1,
         channels: 3
       }
-    }).metadata().then(info => {
-      assert.strictEqual(info.width, 2);
-      assert.strictEqual(info.height, 1);
-      done();
-    });
-  });
-
-  it('should create a simple image from rgb values', (done) => {
-    const uint8array = Uint8Array.from([255, 255, 255, 0, 0, 0]);
-    sharp(uint8array, {
-      raw: {
-        width: 2,
-        height: 1,
-        channels: 3
-      }
-    }).metadata().then(info => {
-      assert.strictEqual(info.width, 2);
-      assert.strictEqual(info.height, 1);
-      done();
-    });
-  });
-
-  it('data should be same going in and out', (done) => {
-    const input = Uint8Array.from([1, 1, 1, 2, 2, 2]);
-    const image = sharp(input, {
-      raw: {
-        width: 2,
-        height: 1,
-        channels: 3
-      }
-    });
-    image.toUint8Array().then((output) => {
-      assert.deepStrictEqual(input, output);
-      done();
-    });
+    }).toUint8Array({ resolveWithObject: true });
+    
+    assert.deepStrictEqual(uint8array, new Uint8ClampedArray(data));
+    assert.strictEqual(info.width, 2);
+    assert.strictEqual(info.height, 1);
   });
 });
