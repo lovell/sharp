bdb1986e08f11366e9ca16ed26fc26de7a073b24
Lovell Fuller <github@lovell.info>
2021-03-17 23:25:34 +0000

    Tests: run in parallel again

--- test/unit/toFormat.js
+++ test/unit/toFormat.js
@@ -5,22 +5,23 @@ const sharp = require('../../');
 const fixtures = require('../fixtures');
 
 describe('toFormat', () => {
-  it('accepts upper case characters as format parameter (string)', function (done) {
-    sharp(fixtures.inputJpg)
+  it('accepts upper case characters as format parameter (string)', async () => {
+    const data = await sharp(fixtures.inputJpg)
+      .resize(8, 8)
       .toFormat('PNG')
-      .toBuffer(function (err, data, info) {
-        if (err) throw err;
-        assert.strictEqual('png', info.format);
-        done();
-      });
+      .toBuffer();
+
+    const { format } = await sharp(data).metadata();
+    assert.strictEqual(format, 'png');
   });
-  it('accepts upper case characters as format parameter (object)', function (done) {
-    sharp(fixtures.inputJpg)
+
+  it('accepts upper case characters as format parameter (object)', async () => {
+    const data = await sharp(fixtures.inputJpg)
+      .resize(8, 8)
       .toFormat({ id: 'PNG' })
-      .toBuffer(function (err, data, info) {
-        if (err) throw err;
-        assert.strictEqual('png', info.format);
-        done();
-      });
+      .toBuffer();
+
+    const { format } = await sharp(data).metadata();
+    assert.strictEqual(format, 'png');
   });
 });
