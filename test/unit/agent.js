28351a928b781ca7cbda77886810479374d5d3fe
a374459 <matthieu.salettes@socgen.com>
2021-04-15 21:38:20 +0200

    fix proxy authentication when credentials contain special character

--- test/unit/agent.js
+++ test/unit/agent.js
@@ -19,6 +19,17 @@ describe('HTTP agent', function () {
     assert.strictEqual(443, proxy.defaultPort);
   });
 
+  it('HTTPS proxy with auth from HTTPS_PROXY using credentials containing special characters', function () {
+    process.env.HTTPS_PROXY = 'https://user,:pass=@secure:123';
+    const proxy = agent();
+    delete process.env.HTTPS_PROXY;
+    assert.strictEqual('object', typeof proxy);
+    assert.strictEqual('secure', proxy.options.proxy.host);
+    assert.strictEqual(123, proxy.options.proxy.port);
+    assert.strictEqual('user,:pass=', proxy.options.proxy.proxyAuth);
+    assert.strictEqual(443, proxy.defaultPort);
+  });
+
   it('HTTP proxy without auth from npm_config_proxy', function () {
     process.env.npm_config_proxy = 'http://plaintext:456';
     const proxy = agent();
