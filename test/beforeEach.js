4490a93430e5e85b03ed122bbde487c319e3acef
Lovell Fuller <github@lovell.info>
2022-11-15 09:27:09 +0000

    Tests: simplify beforeEach configuration
    Remove legacy settings for previous CI providers/hardware

--- test/beforeEach.js
+++ test/beforeEach.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const detectLibc = require('detect-libc');
 const sharp = require('../');
 
-const libcFamily = detectLibc.familySync();
-const usingCache = !(process.env.G_DEBUG || libcFamily === detectLibc.MUSL);
-const usingSimd = !(process.env.G_DEBUG || process.env.VIPS_NOVECTOR);
-const concurrency = process.env.VIPS_CONCURRENCY ||
-  (libcFamily === detectLibc.MUSL || process.arch === 'arm' ? 1 : undefined);
+const usingCache = !process.env.G_DEBUG;
+const usingSimd = !process.env.VIPS_NOVECTOR;
+const concurrency = Number(process.env.VIPS_CONCURRENCY) || 0;
 
 exports.mochaHooks = {
   beforeEach () {
