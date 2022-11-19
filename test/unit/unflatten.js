64a2202c43c0a41c80a028dd2d1e8f17d7f7de36
Anton Marsden <anton.marsden@ninetyten.co.nz>
2022-11-19 13:44:15 +1300

    test case updates

--- test/unit/unflatten.js
+++ test/unit/unflatten.js
@@ -7,7 +7,7 @@ const fixtures = require('../fixtures');
 
 describe('Unflatten', function () {
   it('unflatten white background', function (done) {
-    sharp(fixtures.inputPng).unflatten({ thresholds: [255] })
+    sharp(fixtures.inputPng).unflatten()
       .toBuffer(function (err, data) {
         if (err) throw err;
         fixtures.assertSimilar(fixtures.expected('unflatten-white-transparent.png'), data, { threshold: 0 }, done);
@@ -18,6 +18,13 @@ describe('Unflatten', function () {
       .toBuffer(function (err, data) {
         if (err) throw err;
         fixtures.assertSimilar(fixtures.expected('unflatten-swiss.png'), data, { threshold: 0 }, done);
+      });
+  });
+  it('no unflatten', function (done) {
+    sharp(fixtures.inputPng).unflatten(false)
+      .toBuffer(function (err, data) {
+        if (err) throw err;
+        fixtures.assertSimilar(fixtures.inputPng, data, { threshold: 0 }, done);
       });
   });
 });
