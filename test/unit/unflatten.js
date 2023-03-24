39ac7c95e000a3f5f69f92825c8eaef16ad59bf9
Anton Marsden <anton.marsden@ninetyten.co.nz>
2023-03-25 12:56:35 +1300

    test improvement

--- test/unit/unflatten.js
+++ test/unit/unflatten.js
@@ -21,7 +21,7 @@ describe('Unflatten', function () {
       });
   });
   it('unflatten using threshold', function (done) {
-    sharp(fixtures.inputPngPalette).unflatten().threshold(128, { grayscale: false })
+    sharp(fixtures.inputPngPalette).unflatten(true).threshold(128, { grayscale: false })
       .toBuffer(function (err, data) {
         if (err) throw err;
         fixtures.assertSimilar(fixtures.expected('unflatten-swiss.png'), data, { threshold: 0 }, done);
