06fa41ff2b9f39ff837555353ca1b8d26329918c
Ingvar Stepanyan <me@rreverser.com>
2023-06-30 14:28:08 +0100

    One more fixup

--- install/noop-wasm.js
+++ install/noop-wasm.js
@@ -1,4 +1,4 @@
 // Stops the `install` command if we just want to use prebuilt Wasm assets, proceeds to the node-gyp compilation otherwise.
-if (!process.env.npm_config_arch.startsWith('wasm') || process.env.npm_config_build_from_source) {
+if (!process.env.npm_config_arch?.startsWith('wasm') || process.env.npm_config_build_from_source) {
   process.exit(1);
 }
