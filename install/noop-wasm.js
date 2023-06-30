e110f06af0d2a31ce31c980f6f550d569c8e5b48
Ingvar Stepanyan <me@rreverser.com>
2023-06-30 14:29:55 +0100

    One more fix for Stackblitz

--- install/noop-wasm.js
+++ install/noop-wasm.js
@@ -1,4 +1,8 @@
 // Stops the `install` command if we just want to use prebuilt Wasm assets, proceeds to the node-gyp compilation otherwise.
-if (!process.env.npm_config_arch?.startsWith('wasm') || process.env.npm_config_build_from_source) {
+if (
+  !process.env.npm_config_arch?.startsWith("wasm") ||
+  "webcontainer" in process.versions ||
+  process.env.npm_config_build_from_source
+) {
   process.exit(1);
 }
