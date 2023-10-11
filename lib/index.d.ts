4cb9188902e00b6ce587c968abfc89e34e9bcac7
Thomas Way <thomas@6f.io>
2023-10-11 17:25:50 +0100

    fix: allow boolean withMetadata argument
    Fixes: #3822

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -646,7 +646,7 @@ declare namespace sharp {
          * @param withMetadata
          * @throws {Error} Invalid parameters.
          */
-        withMetadata(withMetadata?: WriteableMetadata): Sharp;
+        withMetadata(withMetadata?: boolean | WriteableMetadata): Sharp;
 
         /**
          * Use these JPEG options for output image.
@@ -1282,10 +1282,10 @@ declare namespace sharp {
     }
 
     interface NormaliseOptions {
-      /** Percentile below which luminance values will be underexposed. */
-      lower?: number | undefined;
-      /** Percentile above which luminance values will be overexposed. */
-      upper?: number | undefined;
+        /** Percentile below which luminance values will be underexposed. */
+        lower?: number | undefined;
+        /** Percentile above which luminance values will be overexposed. */
+        upper?: number | undefined;
     }
 
     interface ResizeOptions {
--- test/types/sharp.test-d.ts
+++ test/types/sharp.test-d.ts
@@ -661,3 +661,6 @@ sharp('input.tiff').webp({ preset: 'drawing' }).toFile('out.webp');
 sharp('input.tiff').webp({ preset: 'text' }).toFile('out.webp');
 sharp('input.tiff').webp({ preset: 'default' }).toFile('out.webp');
 
+// Allow a boolean or an object for metadata options.
+// https://github.com/lovell/sharp/issues/3822
+sharp(input).withMetadata().withMetadata({}).withMetadata(false);
