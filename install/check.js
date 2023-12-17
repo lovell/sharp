f28e79ef4f853496b435aaef2f719eb1092a54d6
Lovell Fuller <github@lovell.info>
2023-12-17 09:46:28 +0000

    Improve resilience of installation check

--- install/check.js
+++ install/check.js
@@ -3,34 +3,39 @@
 
 'use strict';
 
-const { useGlobalLibvips, globalLibvipsVersion, log, spawnRebuild } = require('../lib/libvips');
+try {
+  const { useGlobalLibvips, globalLibvipsVersion, log, spawnRebuild } = require('../lib/libvips');
 
-const buildFromSource = (msg) => {
-  log(msg);
-  log('Attempting to build from source via node-gyp');
-  try {
-    require('node-addon-api');
-    log('Found node-addon-api');
-  } catch (err) {
-    log('Please add node-addon-api to your dependencies');
-    return;
-  }
-  try {
-    const gyp = require('node-gyp');
-    log(`Found node-gyp version ${gyp().version}`);
-  } catch (err) {
-    log('Please add node-gyp to your dependencies');
-    return;
-  }
-  log('See https://sharp.pixelplumbing.com/install#building-from-source');
-  const status = spawnRebuild();
-  if (status !== 0) {
-    process.exit(status);
-  }
-};
+  const buildFromSource = (msg) => {
+    log(msg);
+    log('Attempting to build from source via node-gyp');
+    try {
+      require('node-addon-api');
+      log('Found node-addon-api');
+    } catch (err) {
+      log('Please add node-addon-api to your dependencies');
+      return;
+    }
+    try {
+      const gyp = require('node-gyp');
+      log(`Found node-gyp version ${gyp().version}`);
+    } catch (err) {
+      log('Please add node-gyp to your dependencies');
+      return;
+    }
+    log('See https://sharp.pixelplumbing.com/install#building-from-source');
+    const status = spawnRebuild();
+    if (status !== 0) {
+      process.exit(status);
+    }
+  };
 
-if (useGlobalLibvips()) {
-  buildFromSource(`Detected globally-installed libvips v${globalLibvipsVersion()}`);
-} else if (process.env.npm_config_build_from_source) {
-  buildFromSource('Detected --build-from-source flag');
+  if (useGlobalLibvips()) {
+    buildFromSource(`Detected globally-installed libvips v${globalLibvipsVersion()}`);
+  } else if (process.env.npm_config_build_from_source) {
+    buildFromSource('Detected --build-from-source flag');
+  }
+} catch (err) {
+  const summary = err.message.split(/\n/).slice(0, 1);
+  console.log(`sharp: skipping install check: ${summary}`);
 }
