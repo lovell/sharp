2346722c0de1ee1a2fb4524a1607c8e23772bb26
Lovell Fuller <github@lovell.info>
2026-01-02 10:58:18 +0000

    Upgrade sharp-libvips to v1.3.0-rc.2

--- test/unit/libvips.js
+++ test/unit/libvips.js
@@ -180,7 +180,7 @@ describe('libvips binaries', () => {
       process.env.npm_config_arch = 's390x';
       process.env.npm_config_libc = '';
       const locatorHash = libvips.yarnLocator();
-      assert.strictEqual(locatorHash, '8cdba194cb');
+      assert.strictEqual(locatorHash, 'f7e557e9d6');
       delete process.env.npm_config_platform;
       delete process.env.npm_config_arch;
       delete process.env.npm_config_libc;
