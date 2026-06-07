d1521d25f89cb75071207b4d0c2e7bca8cd5b038
Lovell Fuller <github@lovell.info>
2026-06-07 09:00:10 +0100

    Tests: ensure ESM test uses file scheme on Windows

--- test/unit/esm.mjs
+++ test/unit/esm.mjs
@@ -1,13 +1,14 @@
 import { createRequire } from 'node:module';
 import { resolve } from 'node:path';
 import { suite, test } from 'node:test';
+import { pathToFileURL } from 'node:url';
 
 import pkg from '../../package.json' with { type: 'json' };
 
 suite('ESM', () => {
   test('await import', async (t) => {
     t.plan(1);
-    const sharp = await import(resolve(pkg.module));
+    const sharp = await import(pathToFileURL(resolve(pkg.module)));
     t.assert.deepStrictEqual(typeof sharp.default.versions, 'object');
   });
 
