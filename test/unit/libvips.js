a82a0b3d58bc25854ad1e925e6eb0a50725d1489
Lovell Fuller <github@lovell.info>
2026-08-25 22:16:32 +0100

    Upgrade to libvips v8.18.6

--- test/unit/libvips.js
+++ test/unit/libvips.js
@@ -198,7 +198,7 @@ suite('libvips binaries', () => {
       process.env.npm_config_arch = 's390x';
       process.env.npm_config_libc = '';
       const locatorHash = libvips.yarnLocator();
-      t.assert.strictEqual(locatorHash, '9fe80dfa43');
+      t.assert.strictEqual(locatorHash, '06e98857ae');
       delete process.env.npm_config_platform;
       delete process.env.npm_config_arch;
       delete process.env.npm_config_libc;
